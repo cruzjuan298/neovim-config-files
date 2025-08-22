f715f1bf7cf063be50d2abe45ea5907fc9e7c60a
Juan Cruz <cruzjuan298@gmail.com>
2025-08-22 03:23:30 -0400

    initial commit of my neovim config files

---
+++ plugins.lua
@@ -0,0 +1,129 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("lazy").setup({
+  spec = {
+        {
+          "bluz71/vim-moonfly-colors",
+            config = function()
+                vim.cmd.colorscheme("moonfly")
+            end,
+        },
+        {                                      
+        "nvim-treesitter/nvim-treesitter",  
+            branch = "master",                  
+            lazy = false,
+            build = ":TSUpdate",
+            config = function()
+                require("nvim-treesitter.configs").setup({
+                    highlight = {
+                        enable = true,
+                    },
+                    indent = {
+                        enable = true,
+                    },
+
+                    ensure_installed = {
+                        "lua", 
+                        "vim", 
+                        "python", 
+                        "javascript",
+                        "c",
+                        "vimdoc",
+                        "query"
+                    },
+                    
+                    auto_install = true,
+
+                    incremental_selection = {
+                        enable = true,
+                            keymaps = {
+                                init_selection = "<Leader>ss", 
+                                node_incremental = "<Leader>si",
+                                scope_incremental = "<Leader>sc",
+                                node_decremental = "<Leader>sd",
+                            },
+                    },
+
+                })
+            end,
+        },
+
+        {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            config = function()
+                    require'nvim-treesitter.configs'.setup {
+                      textobjects = {
+                        select = {
+                            enable = true,
+                            lookahead = true,
+
+                            keymaps = { 
+                                ["af"] = "@function.outer",
+                                ["if"] = "@function.inner",
+                                ["ac"] = "@class.outer",        
+                                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                                ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+                            },
+                            selection_modes = {
+                                ['@parameter.outer'] = 'v', 
+                                ['@function.outer'] = 'V', 
+                                ['@class.outer'] = '<c-v>', 
+                            }, 
+                          include_surrounding_whitespace = true,
+                        },
+                      },
+                    }
+                end,
+        },  
+        
+        {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+
+        {
+            "neovim/nvim-lspconfig",
+        },
+
+        {
+            "mason-org/mason.nvim",
+            config = function()
+                require("mason").setup()
+            end
+        },
+        
+        {
+            "mason-org/mason-lspconfig.nvim",
+            dependencies = {"mason.nvim"},
+            config = function() 
+                require("mason-lspconfig").setup({
+                    handlers = {
+                        function(server_name)
+                            require("lspconfig")[server_name].setup({})
+                        end,    
+                    },
+                })
+            end,
+        }
+
+    },    
+  install = { colorscheme = { "habamax" } },
+  checker = { enabled = true },
+    
+})
